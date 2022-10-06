29d976141d212bc0ca46ebcf8738562522bb3579
schu-code <sophiachu2@gmail.com>
2022-10-05 17:30:23 -0700

    Initlial setup

--- uno/api/app/middleware/log_request.js
+++
@@ -1,8 +0,0 @@
-/**
- * This middleware logs requests made to the API.
- */
-
-module.exports = function(req, res, next) {
-    req.app.get("logger").http(`[${res.statusCode}] ${req.method} ${req.originalUrl}`);
-    next();
-};
---
+++ uno/config/config.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+console.log(`*** [${process.env.DATABASE_URL}] ***`);
+
+module.exports = {
+  "development": {
+    "use_env_variable": "DATABASE_URL",
+    "dialect": "postgres"
+  },
+  "test": {
+    "use_env_variable": "DATABASE_URL",
+    "dialect": "postgres"
+  },
+  "production": {
+    "use_env_variable": "DATABASE_URL",
+    "dialect": "postgres"
+  }
+}
