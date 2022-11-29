1810254b53519cc499103c309aef2c7873b9a37c
troyc529 <troy.carloni@gmail.com>
2022-11-29 15:17:46 -0800

    global chat, username shows up however getting odd behavior where usernames will display other users.

--- uno/public/scripts/g_chat_frontend.js
+++ uno/public/scripts/g_chat_frontend.js
@@ -8,12 +8,25 @@ const socket = io({
 messageButton.addEventListener('click', addMessage);
 
 socket.on('message',(data) => {
-    message_container.innerHTML += `
-      
-        <p> ${data.message}<p>
-   
-        `;
+    message_container.innerHTML += createContainer(data.username, data.message)
 })
+
+function createContainer(username, message){
+    return `
+<div class="row comments mb-2">
+<div class="col-md-2 col-sm-2 col-3 text-center user-img">
+<p>&nbsp</p>
+</div>
+<div class="col-md-9 col-sm-9 col-9 comment rounded mb-2">
+<h4 class="m-0"><a href="#">${username}</a></h4>
+<time class="text-white ml-3"></time>
+<like></like>
+<p class="mb-0 text-white">${message}</p>
+</div>
+</div>
+
+`
+}
 
 function addMessage () {
     if(input.value === '') {
