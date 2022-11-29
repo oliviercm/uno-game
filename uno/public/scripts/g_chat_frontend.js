6f71bc69693cdedb69157104f03151b5b18270c1
troyc529 <troy.carloni@gmail.com>
2022-11-29 14:47:09 -0800

    added global chat front end. Any text sent in chat window lobby will show up to all members logged in on that page

--- uno/public/scripts/g_chat_frontend.js
+++ uno/public/scripts/g_chat_frontend.js
@@ -1,15 +1,19 @@
 const message_container = document.querySelector('.chat-field')
 const messageButton = document.querySelector('.input-button')
 const input = document.querySelector('.input-field-chat')
-const socket = io()
-
-
-socket.on('chat-message', data => {
-    console.log(data)
-})
+const socket = io({
+    path: "/global-chat/"
+});
 
 messageButton.addEventListener('click', addMessage);
 
+socket.on('message',(data) => {
+    message_container.innerHTML += `
+      
+        <p> ${data.message}<p>
+   
+        `;
+})
 
 function addMessage () {
     if(input.value === '') {
@@ -28,15 +32,7 @@ function addMessage () {
             })
         }
         fetch('/api/global-chat', message)
-        .then((response)=>{
-            console.log("HERE")
-            console.log(response)
-        })
-        message_container.innerHTML += `
-      
-        <p> ${input.value}<p>
-   
-        `;
+       
     input.value = '';
     input.focus();
     }
